75a6984136cc5c647622af53fd5f2311074eb8ec
Luong Huu Thanh <117010350+fisherman611@users.noreply.github.com>
2024-11-23 22:44:20 +0700

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,15 +1,18 @@
-import segmentation_models_pytorch as smp 
-import os 
-import sys 
+import segmentation_models_pytorch as smp
+import os
+import sys
 
+# Add the 'utils' directory to the system path for importing custom modules
 sys.path.append('/'.join(os.getcwd().split('\\')[:2]) + '/utils')
 
-from log import * 
+# Import logging utilities (custom module)
+from log import *
 
-# I will use the UNet model using the segmentation_models_pytorch library built on Pytorch framework
+# Initialize the UNet model from the segmentation_models_pytorch library.
+# This library is built on top of the PyTorch framework for semantic segmentation tasks.
 unet_model = smp.Unet(
-    encoder_name=ENCODER_NAME,        
-    encoder_weights=ENCODER_WEIGHTS,     
-    in_channels=IN_CHANNELS,                  
-    classes=N_CLASSES 
-    )
+    encoder_name=ENCODER_NAME,        # Name of the encoder (e.g., 'resnet34', 'efficientnet-b0')
+    encoder_weights=ENCODER_WEIGHTS, # Pre-trained weights for the encoder (e.g., 'imagenet' or None)
+    in_channels=IN_CHANNELS,         # Number of input channels (e.g., 3 for RGB images)
+    classes=N_CLASSES                # Number of output segmentation classes
+)
